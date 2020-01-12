f96c7195c16decf3e2ac174595ce47020f88e410
hodgs068 <willhodgson2004@gmail.com>
2020-01-12 13:48:59 -0600

    Updates

---
+++ precache-manifest.b4597d957cc0e8e6184777bdb5f6daa8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "047037c730481e249ab9eeb855e04218",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "5d8baab92528cafab5d2",
+    "url": "/myapp/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "31e9892722ad42d68a26",
+    "url": "/myapp/static/js/2.66ae91d0.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/myapp/static/js/2.66ae91d0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5d8baab92528cafab5d2",
+    "url": "/myapp/static/js/main.b5091d63.chunk.js"
+  },
+  {
+    "revision": "9566c43c088d2c0db93f",
+    "url": "/myapp/static/js/runtime-main.816e65f5.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/myapp/static/media/logo.ee7cd8ed.svg"
+  }
+]);
